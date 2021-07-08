ec57a2d8634fccf4b011fffa20d487aa225eeaf4
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-08 17:14:46 +0200

    Card Bootstrap with Title of the Music

--- src/components/ListCharacter.vue
+++ src/components/ListCharacter.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="row">
-    <div class="col-6 col-md-4 col-lg-3">Prova</div>
+    <div
+      v-for="(item, index) in listCharacter"
+      :key="index"
+      class="col-6 col-md-4 col-lg-3"
+    >
+      <div class="card" style="width: 18rem">
+        <img src="@/poster" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{{ item.title }}</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,11 +45,17 @@ export default {
   },
   methods: {
     getCharacters() {
-      axios.get(this.apiURL).then((res) => {
-        // console.log(res.data);
-        this.listCharacter = res.data;
-        console.log(this.listCharacter);
-      });
+      axios
+        .get(this.apiURL)
+        .then((res) => {
+          // Creo un array di oggetti che è uguale all'array dell'API che mi sono andato a collegare
+          // console.log(res.data);
+          this.listCharacter = res.data.response;
+          console.log(this.listCharacter);
+        })
+        .catch((error) => {
+          console.log("Errore:", error);
+        });
     },
   },
 };
